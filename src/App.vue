28bc629daf08e71ea428ad0840cc9b1d47567a63
Moises Baltazar <me@moisesb.com>
2020-02-02 20:05:18 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,144 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="wrapper">
+    <nav class="uk-navbar-container" uk-navbar style="height: 50px">
+      <div class="uk-navbar-center">
+        <ul class="uk-navbar-nav">
+          <li class="uk-disabled" v-bind:class="isActive('servers')">
+            <router-link to="/servers">1. Servers</router-link>
+          </li>
+          <li class="uk-disabled" v-bind:class="isActive('options')">
+            <router-link to="/options">2. Options</router-link>
+          </li>
+          <li class="uk-disabled" v-bind:class="isActive('upload')">
+            <router-link to="/upload">3. Upload</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="content">
+      <router-view></router-view>
     </div>
-    <router-view />
+
+    <div class="footer">
+      <div class="uk-align-left" style="margin: 5px">
+        <!-- Questions button -->
+        <button
+          class="uk-button uk-button-default"
+          uk-toggle="target: #questions-modal"
+          style="width:20px"
+        >
+          <i
+            class="fa fa-question fa-2x"
+            aria-hidden="true"
+            style="position:relative; top:5px; left:-10px; color:gray"
+          ></i>
+        </button>
+      </div>
+      <div class="uk-align-right" style="margin: 5px">
+        <button
+          class="uk-button uk-button-danger app-button"
+          style="width:110px"
+          @click="exit"
+        >
+          Exit
+        </button>
+        <button
+          class="uk-button uk-button-default"
+          id="back-btn"
+          style="width:110px"
+          v-bind:disabled="this.$route.name === 'servers'"
+          @click="back"
+        >
+          Back
+        </button>
+        <button
+          class="uk-button uk-button-primary"
+          id="next-btn"
+          style="width:110px"
+          @click="next"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+
+    <!-- Questions modal -->
+    <div id="questions-modal" uk-modal>
+      <div class="uk-modal-dialog uk-modal-body">
+        <h2 class="uk-modal-title">Uploader Help</h2>
+        <p>
+          If you have any questions or find any bugs in the uploader, please
+          contact
+          <a :href="'mailto:' + adminEmail">{{ adminEmail }}</a
+          >.
+        </p>
+        <p class="uk-text-right">
+          <button
+            class="uk-button uk-button-default uk-modal-close"
+            type="button"
+          >
+            OK
+          </button>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+const app = require("electron").remote.app;
+const settings = require("electron-settings");
 
-#nav {
-  padding: 30px;
+export default {
+  data() {
+    return {
+      transferClient: "",
+      adminEmail: app.adminEmail
+    };
+  },
+  beforeCreate() {
+    this.$router.push({ name: "servers" });
+    console.log("\ninside App.vue src directory:");
+    console.log(__dirname + "\n");
+  },
+  methods: {
+    exit: function() {
+      // Clear some settings we don't want to keep between runs
+      settings.set("options.projects", []);
+      settings.set("options.subjectLabel", "");
+      settings.set("options.sessionLabel", "");
+      settings.set("config.password", "");
+      window.close();
+    },
+    back: function() {
+      // console.log("<-- going back")
+      if (this.$route.name == "options") {
+        this.$router.push({ name: "servers" });
+      } else if (this.$route.name == "upload") {
+        this.$router.push({ name: "options" });
+      }
+    },
+    next: function() {
+      // console.log("going forward -->")
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+      if (this.$route.name == "servers") {
+        this.$router.push({ name: "options" });
+      } else if (this.$route.name == "options") {
+        this.$router.push({ name: "upload" });
+      }
+    },
+    isActive: function(link) {
+      return {
+        "uk-active": this.$route.name === link
+      };
     }
   }
+};
+</script>
+
+<style>
+uk-active: {
+  font-weight: 900;
 }
 </style>
